8395f7182e0955b147a5be008581cccbd3b8c4e0
temamironovonline <a.sergeichev03@mail.ru>
2022-10-03 18:20:14 +0300

    awd

--- Lists/main.c
+++ Lists/main.c
@@ -12,6 +12,8 @@ LOS* create(int);
 void show(LOS*);
 void delete(LOS*);
 void add(LOS*, LOS, int);
+void deleteItem(LOS*, int);
+void changeItems(LOS*, int, int);
 
 
 int main(void)
@@ -23,8 +25,16 @@ int main(void)
 	LOS str = {25, NULL};
 	add(list, str, 5);
 	show(list);
+	printf("\n");
 	delete(list);
-	
+	list = create(10);
+	show(list);
+	printf("\n");
+	deleteItem(list, 2);
+	show(list);
+	printf("\n");
+	changeItems(list, 1, 2);
+	show(list);
 }
 
 LOS* create(int n)
@@ -77,4 +87,93 @@ void add(LOS* list, LOS item, int k)
 	void* address = list->next;
 	list->next = element;
 	element->next = address;
+}
+
+void deleteItem(LOS* list, int k)
+{
+	LOS* element = list;
+	for (int i = 0; i < k; i++)
+	{
+		list = list->next;
+	}
+	void* address = list->next;
+	free(list);
+	list = element;
+	for (int i = 0; i < k-1; i++)
+	{
+		list = list->next;
+	}
+	list->next = address;
+}
+
+void changeItems(LOS* list, int a, int b)
+{
+	LOS* element = list;
+	void* addressB = NULL;
+	void* addressAfterB = NULL;
+	void* addressA = NULL;
+	void* addressAfterA = NULL;
+	if (a > b)
+	{
+		int c = b;
+		a = b;
+		b = c;
+	}
+	if (a == 1 && b == 2)
+	{
+			addressA = list;
+			addressB = list->next;
+			list = list->next;
+			addressAfterB = list->next;
+			list = addressB;
+			list->next = addressA;
+			list = list->next;
+			list->next = addressAfterB;
+	}
+	else
+	{
+
+		for (int i = 0; i < b; i++)
+		{
+			list = list->next;
+			if (i == b - 2)
+			{
+				addressB = list->next;
+			}
+			if (i == b - 1)
+			{
+				addressAfterB = list->next;
+			}
+			if (i == a - 2)
+			{
+				addressA = list->next;
+			}
+			if (i == a - 1)
+			{
+				addressAfterA = list->next;
+			}
+		}
+		list = element;
+		for (int i = 0; i < b; i++)
+		{
+			list = list->next;
+			if (i == a - 2)
+			{
+				list->next = addressB;
+			}
+			if (i == a - 1)
+			{
+				list->next = addressAfterA;
+			}
+			if (i == b - 2)
+			{
+				list->next = addressA;
+			}
+			if (i == b - 1)
+			{
+				list->next = addressAfterB;
+			}
+
+		}
+	}
 }
